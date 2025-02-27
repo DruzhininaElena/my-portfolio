6721ae25832724d6253574193fec075e1e04433e
Elena Druzhinina <elenashebrova@gmail.com>
2025-02-27 16:46:18 +0700

    create footer section

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,16 +1,15 @@
 import styled from 'styled-components';
 import {Icon} from './icon/Icon.tsx';
-import {FlexWrapper} from './FlexWrapper.tsx';
 
 
 export const Social = () => {
     return (
         <StyledSocial>
-            <FlexWrapper justify={'space-around'}>
-                <Icon iconId={'telegram'} height={'40'} width={'40'} viewBox={'3 2 18 18'}/>
-                <Icon iconId={'whatsapp'} height={'40'} width={'40'} viewBox={'-2 -2 23 23'}/>
-                <Icon iconId={'email'} height={'40'} width={'40'} viewBox={'-2 -8 45 45'}/>
-            </FlexWrapper>
+            <ul>
+                <li><a href="#"><Icon iconId={'telegram'} height={'40'} width={'40'} viewBox={'3 2 18 18'}/></a></li>
+                <li><a href="#"><Icon iconId={'whatsapp'} height={'40'} width={'40'} viewBox={'-2 -2 23 23'}/></a></li>
+                <li><a href="#"><Icon iconId={'email'} height={'40'} width={'40'} viewBox={'-2 -8 45 45'}/></a></li>
+            </ul>
         </StyledSocial>
     );
 };
@@ -18,5 +17,9 @@ export const Social = () => {
 const StyledSocial = styled.div`
     margin: 0 auto;
     max-width: 200px;
+    ul {
+        display: flex;
+        justify-content: space-around;
+    }
 `
 
