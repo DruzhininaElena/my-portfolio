04502079b17a69a2d01ec4171d288740dcb6f9ce
Elena Druzhinina <elenashebrova@gmail.com>
2025-03-09 18:13:52 +0700

    Updates

--- assets/index-BEPZ-XPq.js
+++ assets/index-BEPZ-XPq.js
@@ -246,7 +246,7 @@ Error generating stack: `+n.message+`
     
 `,Iy=""+new URL("photoMy-AgdFpN68.webp",import.meta.url).href,$y=mt.div`
     width: 50%;
-    transform: translate(0, -28%);
+    transform: translate(8%, -28%);
 
     @media ${st.media.desktopLg} {
         width: 60%;
@@ -262,7 +262,7 @@ Error generating stack: `+n.message+`
     }
 
     @media ${st.media.mobile} {
-        width: 100%;
+        width: 110%;
         transform: translate(0%, -20%);
     }
 `,Jy={PhotoWrapper:$y},Wy=()=>G.jsx(Jy.PhotoWrapper,{children:G.jsxs("svg",{viewBox:"0 0 777 877",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:[G.jsx("path",{d:"M426.75 0.66C526.01 5.37 630.41 42.26 691.01 119.05C747.05 190.06 709.4 290.88 725.4 379.11C738.27 450.04 786.3 512.96 775.4 584.2C763.33 663.13 724.84 738.45 662.53 790.18C597.27 844.36 510.83 889.89 426.75 873.67C345.26 857.94 319.87 754.82 250.24 710.63C174.06 662.28 41.25 688.78 6.43 606.91C-27.69 526.71 83.95 456.25 106.3 372.2C131.06 279.1 79.6 166.63 143.09 93.02C208.86 16.76 324.68 -4.19 426.75 0.66Z",fill:"#FDC435"}),G.jsx("pattern",{id:"pattern",width:"100%",height:"100%",patternTransform:"translate(200 360)",children:G.jsx("image",{width:"60%",height:"60%",xlinkHref:Iy,preserveAspectRatio:"xMidYMid meet"})}),G.jsx("path",{d:"M426.75 0.66C526.01 5.37 630.41 42.26 691.01 119.05C747.05 190.06 709.4 290.88 725.4 379.11C738.27 450.04 786.3 512.96 775.4 584.2C763.33 663.13 724.84 738.45 662.53 790.18C597.27 844.36 510.83 889.89 426.75 873.67C345.26 857.94 319.87 754.82 250.24 710.63C174.06 662.28 41.25 688.78 6.43 606.91C-27.69 526.71 83.95 456.25 106.3 372.2C131.06 279.1 79.6 166.63 143.09 93.02C208.86 16.76 324.68 -4.19 426.75 0.66Z",fill:"url(#pattern)"})]})}),ky=mt.section`
