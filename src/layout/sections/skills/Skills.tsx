d5b3902f0e866543d5c8111a1c637dfff2db6f90
Elena Druzhinina <elenashebrova@gmail.com>
2025-03-10 17:40:55 +0700

    add animation to skills section

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -4,6 +4,8 @@ import {SkillsData} from './SkillsData.ts';
 import {Container} from '../../../components/container/Container.ts';
 import {S} from './Skills_Styles.ts';
 import React from 'react';
+import { Fade } from "react-awesome-reveal";
+
 
 export const Skills: React.FC = () => {
     return (
@@ -11,7 +13,11 @@ export const Skills: React.FC = () => {
             <Container>
                 <SectionTitle title={'Skills'}/>
                 <S.GridWrapper>
-                    {SkillsData.map(skill => <Icon {...skill} key={skill.iconId}/>)}
+                    <Fade cascade
+                          damping={0.1}
+                          direction={'up'}>
+                        {SkillsData.map(skill => <Icon {...skill} key={skill.iconId}/>)}
+                    </Fade>
                 </S.GridWrapper>
             </Container>
         </S.StyledSkills>
